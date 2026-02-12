75c3a5e4b4f5cd42d1b41949562fcf7dc13eebab
danielrivasc3-cmd <danielrivasc3@gmail.com>
2026-02-11 21:51:25 -0600

    nuevo libro

--- Programacion 2/Programacion 2/main.cpp
+++ Programacion 2/Programacion 2/main.cpp
@@ -4,6 +4,7 @@ int main() {
 	Libro* libro1 = new Libro(101, "El Principito", "Autor", 1992, 1213141516, 215, "Aventura");
 	Libro* libro2 = new Libro(101, "Edipo Rey", "Autor", 1992, 1213141516, 215, "Aventura");
 	Libro* libro3 = new Libro(101, "Orgullo y Prejuicio", "Autor", 1992, 1213141516, 215, "Aventura");
+	Libro* libro4 = new Libro(101, "Thor", "Autor", 1998, 1213141516, 215, "Aventura y accion");
 
 	Lista<Recurso> recursos;
 
