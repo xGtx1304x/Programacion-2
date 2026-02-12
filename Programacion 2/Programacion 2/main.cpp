a3bdc117af6dbd1b57ae4bf9f95fc59695f0f322
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2026-02-11 21:27:25 -0600

    Ahora 3 libros en main

--- Programacion 2/Programacion 2/main.cpp
+++ Programacion 2/Programacion 2/main.cpp
@@ -3,6 +3,7 @@
 int main() {
 	Libro* libro1 = new Libro(101, "El Principito", "Autor", 1992, 1213141516, 215, "Aventura");
 	Libro* libro2 = new Libro(101, "Edipo Rey", "Autor", 1992, 1213141516, 215, "Aventura");
+	Libro* libro3 = new Libro(101, "Orgullo y Prejuicio", "Autor", 1992, 1213141516, 215, "Aventura");
 
 	Lista<Recurso> recursos;
 
