eb3d1b45800bba102080f1fb55f6e978605e607c
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2026-02-11 21:23:53 -0600

    main crea ahora 2 libros

--- Programacion 2/Programacion 2/main.cpp
+++ Programacion 2/Programacion 2/main.cpp
@@ -1,11 +1,13 @@
 #include "GestorRecursos.h"
 
 int main() {
-	Libro* libro = new Libro(101, "El Principito", "Autor", 1992, 1213141516, 215, "Aventura");
+	Libro* libro1 = new Libro(101, "El Principito", "Autor", 1992, 1213141516, 215, "Aventura");
+	Libro* libro2 = new Libro(101, "Edipo Rey", "Autor", 1992, 1213141516, 215, "Aventura");
 
 	Lista<Recurso> recursos;
 
-	recursos.insertarOrdenado(libro);
+	recursos.insertarOrdenado(libro1);
+	recursos.insertarOrdenado(libro2);
 
 	cout << GestorRecursos::cargarRecursos("Recursos.csv")[0]->toString() << endl;
 
